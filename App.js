5f81ce9861f84c9f3e0cdbc96ba1c13fef5ae6d7
kevinwalsh23 <kevinwalsh23@gmail.com>
2018-12-09 12:59:05 -0500

    navigation 1

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Main from './components/MainComponent';
 
 export default class App extends React.Component {
--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,30 +1,49 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
+//Each component is storing its own state, later implementing redux support and connecting all to redux store
 function RenderDish(props) {
+
     const dish = props.dish;
-
-    if (dish != null) {
-        return(
-            <Card 
+    
+        if (dish != null) {
+            return(
+                <Card
                 featuredTitle={dish.name}
-                image={require('./images/uthappizza.png')}
-                >
-                <Text style={{margin: 10}}>
-                    {dish.description}
-                </Text>
-            </Card>
+                image={require('./images/uthappizza.png')}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
 
+class Dishdetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render(){
+        //getParam allows you get the passed in parameters, followed by fallback option
+        const dishId = this.props.navigation.getParam('dishId', '');
+        //plus means turn string into number
+        return(
+        <RenderDish dish={this.state.dishes[+dishId]} />
         );
     }
-    else {
-        return(<View></View>)
-    }
 }
 
-function DishDetail(props) {
-    return(<RenderDish dish={props.dish} />);
-
-}
-export default DishDetail;
+export default Dishdetail;
